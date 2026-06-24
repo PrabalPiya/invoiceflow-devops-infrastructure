45c58500e08648cbf113cb4d0b0491b74ce453fb
Prabal Piya <blank00154@gmail.com>
2026-06-24 18:09:11 +0545

    fixed nodeport

---
+++ terraform/security-group.tf
@@ -0,0 +1,48 @@
+resource "aws_security_group" "invoiceflow_sg" {
+  name        = "${var.project_name}-sg"
+  description = "Security group for InvoiceFlow K3s EC2"
+
+  ingress {
+    description = "SSH from my IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.ssh_allowed_ip]
+  }
+
+  ingress {
+    description = "Frontend NodePort"
+    from_port   = 30080
+    to_port     = 30080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP optional"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS optional"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg"
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "ssh_allowed_ip" {
+  type = string
+}
+
+variable "github_raw_argocd_app_url" {
+  type = string
+}
